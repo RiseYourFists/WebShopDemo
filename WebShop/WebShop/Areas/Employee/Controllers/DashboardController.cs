829bf4f3910277eb6d8786ab5c74fa53d6b84ec6
Emanuel Shavepeyer <daru12378@gmail.com>
2024-03-24 16:54:20 +0200

    Added:   -Employee Dashboard

---
+++ WebShop/WebShop/Areas/Employee/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿namespace WebShop.App.Areas.Employee.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
+
+    [Area(nameof(Employee))]
+    [Authorize(Roles = "Admin, Employee")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Orders()
+        {
+            return View();
+        }
+    }
+}
