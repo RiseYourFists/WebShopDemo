42a08a7373e8a815f0009c2283048b457c8529d2
Emanuel Shavepeyer <daru12378@gmail.com>
2024-04-05 21:28:09 +0300

    Added:   -CartService Tests

---
+++ WebShop/WebShop.Testing/CartServiceTests.cs
@@ -0,0 +1,196 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebShop.Services.Models.BookShop;
+
+namespace WebShop.Testing
+{
+    using Datasets;
+    using Core.Data;
+    using Core.Contracts;
+    using Core.Repository;
+    using Services.ServiceControllers;
+
+    public class CartServiceTests : BaseTestSetup
+    {
+        private IBookShopRepository bookShopRepository;
+        private CartService _service;
+
+        [SetUp]
+        public void SetUp()
+        {
+            base.Setup<ApplicationDbContext>();
+            bookShopRepository = new BookShopRepository((ApplicationDbContext)context);
+            _service = new CartService(bookShopRepository);
+        }
+
+        [TestCase( "1, 1|2, 1", 2, 75.00)]
+        public async Task GetShopItems_ReturnsCorrectData(string data, int expectedItems, decimal expectedTotalPrice)
+        {
+            var tokens = data.Split('|');
+            var items = new Dictionary<int, int>();
+
+            foreach (var token in tokens)
+            {
+                var tokenData = token.Split(", ");
+                int key = int.Parse(tokenData[0]);
+                int value = int.Parse(tokenData[1]);
+                items[key] = value;
+            }
+
+            await CartServiceDatasetSeeder.SeedFor_GetShopItems_Test(context);
+            var result = await _service.GetShopItems(items);
+
+            Assert.That(result.Count == expectedItems, $"Expected: {expectedItems}.\r\n But got: {result.Count}");
+            Assert.That(result.Sum(r => r.TotalPrice) == expectedTotalPrice, $"Expected: {expectedTotalPrice}\r\n But got: {result.Sum(r => r.TotalPrice)}");
+        }
+
+        [TestCase("1, 2|2, 1", 100.00)]
+        public async Task GetTotalPrice_ReturnsCorrectValue(string data, decimal expectedTotalPrice)
+        {
+            var tokens = data.Split('|');
+            var items = new Dictionary<int, int>();
+
+            foreach (var token in tokens)
+            {
+                var tokenData = token.Split(", ");
+                int key = int.Parse(tokenData[0]);
+                int value = int.Parse(tokenData[1]);
+                items[key] = value;
+            }
+
+            await CartServiceDatasetSeeder.SeedFor_GetShopItems_Test(context);
+            var result = await _service.GetTotalPrice(items);
+
+            Assert.That(result == expectedTotalPrice, $"Expected: {expectedTotalPrice}\r\n But got: {result}");
+        }
+
+        [TestCase("1, 1|2, 1", true)]
+        [TestCase("1, 1000|2, 1000", false)]
+        [TestCase("1, 1|5, 1", false)]
+        public async Task IsCartValid_ReturnsCorrectValue(string data, bool expected)
+        {
+            var tokens = data.Split('|');
+            var items = new Dictionary<int, int>();
+
+            foreach (var token in tokens)
+            {
+                var tokenData = token.Split(", ");
+                int key = int.Parse(tokenData[0]);
+                int value = int.Parse(tokenData[1]);
+                items[key] = value;
+            }
+
+            await CartServiceDatasetSeeder.SeedFor_GetShopItems_Test(context);
+            var result = await _service.IsCartValid(items);
+
+            Assert.That(result == expected, $"Expected: {expected}\r\nBut got: {result}");
+        }
+
+        [Test]
+        public async Task AddNewOrder_ThrowsError_WhenInvalidCart()
+        {
+            await CartServiceDatasetSeeder.SeedFor_AddNewOrder_Test(context);
+
+            var model = new OrderModel()
+            {
+                Address = "Address",
+                City = "City",
+                Country = "Country"
+            };
+
+            var items = new Dictionary<int, int>()
+            {
+                { 0, 1 }
+            };
+
+            var userId = Guid.Parse("07fbc9e3-0d5f-4c5d-a1f7-ef1fd67f33c8");
+
+            Assert.ThrowsAsync<InvalidOperationException>(()=> _service.AddNewOrder(items, model, userId));
+
+            try
+            {
+                await _service.AddNewOrder(items, model, userId);
+            }
+            catch (Exception e)
+            {
+                var expectedMsg = "Invalid cart info!";
+                Assert.That(e.Message == expectedMsg, $"Expected message: {expectedMsg}\r\n But got: {e.Message}");
+            }
+        }
+
+        [Test]
+        public async Task AddNewOrder_AddsCorrectInfo()
+        {
+            await CartServiceDatasetSeeder.SeedFor_AddNewOrder_Test(context);
+
+            var model = new OrderModel()
+            {
+                Address = "Address",
+                City = "City",
+                Country = "Country"
+            };
+
+            var items = new Dictionary<int, int>()
+            {
+                { 1, 2 },
+                { 2, 1 }
+            };
+
+            var userId = Guid.Parse("07fbc9e3-0d5f-4c5d-a1f7-ef1fd67f33c8");
+
+            await _service.AddNewOrder(items, model, userId);
+            var dbContext = (ApplicationDbContext)context;
+            var order = await dbContext.PlacedOrders
+                .Include(o => o.PlacedOrderBooks)
+                .FirstOrDefaultAsync();
+
+            if (order == null)
+            {
+                Assert.Fail("Order wasn't added successfully to the context.");
+            }
+
+            var doesOrderContainItems = order.PlacedOrderBooks.Count(ob => ob.BookId == 1 || ob.BookId == 2) == 2;
+
+            Assert.IsTrue(doesOrderContainItems, "No items were added to the order");
+
+            var doesOrderMatch =
+                model.Address == order.Address &&
+                model.City == order.City &&
+                model.Country == order.Country &&
+                order.IsShipped == false &&
+                order.UserId == userId &&
+                order.PlacedOrderBooks.Sum(pb => pb.SingleItemPrice * pb.Quantity) == 100.00m;
+
+            Assert.IsTrue(doesOrderMatch, "Order info didn't match");
+        }
+
+        [Test]
+        public async Task GetInvoice_ReturnsNull_WhenNotFound()
+        {
+            await CartServiceDatasetSeeder.SeedFor_GetInvoice_Test(context);
+            var result = await _service.GetCurrentInvoice(Guid.Parse("b596e9c4-9dcb-4826-8712-31d6d9b9e4c2"));
+            Assert.IsNull(result, "The result must be null");
+        }
+
+        [Test]
+        public async Task GetInvoice_ReturnsCorrectInfo()
+        {
+            await CartServiceDatasetSeeder.SeedFor_GetInvoice_Test(context);
+            var result = await _service.GetCurrentInvoice(Guid.Parse("07fbc9e3-0d5f-4c5d-a1f7-ef1fd67f33c8"));
+            var dbContext = (ApplicationDbContext)context;
+
+            var actualOrder = dbContext.PlacedOrders
+                .Include(o => o.User)
+                .First();
+
+            var isIdentical =
+                result.City == actualOrder.City &&
+                result.Country == actualOrder.Country &&
+                result.Address == actualOrder.Address &&
+                result.TotalPrice == actualOrder.PlacedOrderBooks.Sum(pb => pb.SingleItemPrice * pb.Quantity) &&
+                result.PhoneNumber == actualOrder.User.PhoneNumber &&
+                result.CustomerName == $"{actualOrder.User.FirstName} {actualOrder.User.LastName}";
+
+            Assert.IsTrue(isIdentical, "The order info isn't matching with the expected.");
+        }
+    }
+}
---
+++ WebShop/WebShop.Testing/Datasets/CartServiceDatasetSeeder.cs
@@ -0,0 +1,188 @@
+﻿
+namespace WebShop.Testing.Datasets
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Core.Models.Identity;
+    using Core.Models.BookShop;
+
+    public static class CartServiceDatasetSeeder
+    {
+        public static async Task SeedFor_GetShopItems_Test(DbContext context)
+        {
+            var promotion = new Promotion()
+            {
+                Id = 1,
+                Name = "Promotion",
+                DiscountPercent = 50,
+                StartDate = DateTime.MinValue,
+                EndDate = DateTime.MaxValue,
+            };
+
+            var authors = new List<Author>()
+            {
+                new()
+                {
+                    Id = 1,
+                    Name = "Author1"
+                },
+                new()
+                {
+                    Id = 2,
+                    Name = "Author2"
+                }
+            };
+
+            promotion.AuthorPromotions.Add(new()
+            {
+                AuthorId = 1,
+                PromotionId = 1
+            });
+
+            var books = new List<Book>()
+            {
+                new()
+                {
+                    Id = 1,
+                    Title = "Book1",
+                    AuthorId = 1,
+                    BookCover = "Empty",
+                    BasePrice = 50m,
+                    Description = "Empty",
+                    GenreId = 1,
+                    StockQuantity = 300,
+                },
+                new() {
+                    Id = 2,
+                    Title = "Book2",
+                    AuthorId = 2,
+                    BookCover = "Empty",
+                    BasePrice = 50m,
+                    Description = "Empty",
+                    GenreId = 2,
+                    StockQuantity = 300,
+                },
+                new() {
+                    Id = 3,
+                    Title = "Book3",
+                    AuthorId = 2,
+                    BookCover = "Empty",
+                    BasePrice = 50m,
+                    Description = "Empty",
+                    GenreId = 2,
+                    StockQuantity = 300,
+                }
+            };
+
+            await context.AddRangeAsync(authors);
+            await context.AddAsync(promotion);
+            await context.AddRangeAsync(books);
+            await context.SaveChangesAsync();
+        }
+
+        public static async Task SeedFor_AddNewOrder_Test(DbContext context)
+        {
+            var user = new ApplicationUser()
+            {
+                Id = Guid.Parse("07fbc9e3-0d5f-4c5d-a1f7-ef1fd67f33c8"),
+                UserName = "User",
+                Email = "Email@main.com",
+                FirstName = "FirstName",
+                LastName = "LastName",
+                IsActive = true
+            };
+            var promotion = new Promotion()
+            {
+                Id = 1,
+                Name = "Promotion",
+                DiscountPercent = 50,
+                StartDate = DateTime.MinValue,
+                EndDate = DateTime.MaxValue,
+            };
+
+            promotion.AuthorPromotions.Add(new()
+            {
+                AuthorId = 1,
+                PromotionId = 1
+            });
+
+            var books = new List<Book>()
+            {
+                new()
+                {
+                    Id = 1,
+                    Title = "Book1",
+                    AuthorId = 1,
+                    BookCover = "Empty",
+                    BasePrice = 50m,
+                    Description = "Empty",
+                    GenreId = 1,
+                    StockQuantity = 300,
+                },
+                new() {
+                    Id = 2,
+                    Title = "Book2",
+                    AuthorId = 2,
+                    BookCover = "Empty",
+                    BasePrice = 50m,
+                    Description = "Empty",
+                    GenreId = 2,
+                    StockQuantity = 300,
+                },
+                new() {
+                    Id = 3,
+                    Title = "Book3",
+                    AuthorId = 2,
+                    BookCover = "Empty",
+                    BasePrice = 50m,
+                    Description = "Empty",
+                    GenreId = 2,
+                    StockQuantity = 300,
+                }
+            };
+
+            await context.AddAsync(user);
+            await context.AddAsync(promotion);
+            await context.AddRangeAsync(books);
+            await context.SaveChangesAsync();
+        }
+
+        public static async Task SeedFor_GetInvoice_Test(DbContext context)
+        {
+            var user = new ApplicationUser()
+            {
+                Id = Guid.Parse("07fbc9e3-0d5f-4c5d-a1f7-ef1fd67f33c8"),
+                UserName = "User",
+                Email = "Email@main.com",
+                FirstName = "FirstName",
+                LastName = "LastName",
+                IsActive = true
+            };
+
+            var order = new PlacedOrder()
+            {
+                Address = "Address",
+                City = "City",
+                Country = "Country",
+                DateFulfilled = null,
+                DatePlaced = DateTime.Now,
+                Id = Guid.Parse("9d14e540-ae61-4a92-bb0e-4f8cf7d4186e"),
+                IsShipped = false,
+                UserId = Guid.Parse("07fbc9e3-0d5f-4c5d-a1f7-ef1fd67f33c8"),
+                PlacedOrderBooks = new List<PlacedOrderBook>()
+                {
+                    new()
+                    {
+                        BookId = 1,
+                        Quantity = 2,
+                        SingleItemPrice = 10.00m
+                    }
+                }
+            };
+
+            await context.AddAsync(order);
+            await context.AddAsync(user);
+            await context.SaveChangesAsync();
+        }
+    }
+}
